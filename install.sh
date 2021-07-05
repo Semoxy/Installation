a1c83d08a4aad637a57add872808d579f410bee0
xImAnton <anton.vogelsang@outlook.de>
2021-07-05 10:53:22 +0200

    added -r arg to secret write echos

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ sudo docker build --tag semoxy/client ./WebClient
 
 # generate secrets
 mkdir secrets
-echo $(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c"${1:-32}") > secrets/mongo_pw.txt
-echo $(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c"${1:-32}") > secrets/pepper.txt
-echo -n "admin" > secrets/mongo_user.txt
-echo -n "mongo.semoxy:27017" > secrets/mongo_addr.txt
+echo -e -n $(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c"${1:-32}") > secrets/mongo_pw.txt
+echo -e -n $(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c"${1:-32}") > secrets/pepper.txt
+echo -e -n "admin" > secrets/mongo_user.txt
+echo -e -n "mongo.semoxy:27017" > secrets/mongo_addr.txt
 
 sudo docker-compose up
