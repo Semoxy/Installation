3a8d72db033168fdfa2fdf58da38c27283504042
xImAnton <anton.vogelsang@outlook.de>
2021-07-05 10:13:56 +0200

    improved update.sh to only rebuild when the git pull pulled something

--- update.sh
+++ update.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-cd WebClient || exit
-git pull
-cd ../Server || exit
-git pull
-cd ..
+cd ./Server || exit
+git pull | grep "Already up to date." > /dev/null
+
+if [ "$?" -eq "0" ]
+then
+  echo "Server is up to date!"
+else
+  sudo docker build --tag semoxy/server ./Server
+fi
+
+cd ./WebClient || exit
+git pull | grep "Already up to date." > /dev/null
+
+if [ "$?" -eq "0" ]
+then
+  echo "WebClient is up to date!"
+else
+  sudo docker build --tag semoxy/client ./WebClient
+fi
 
 sudo docker-compose up
